c1e5cc9111817dd2f2759d1301b5e97a558bd959
Omar Tapia Santacruz <omar.tsc@gmail.com>
2021-04-09 09:52:58 +0200

    fix shop functionality

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -38,14 +38,14 @@ public class Shop {
         
         ProductManager pm = new ProductManager(Locale.UK);
         Product p1 = pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
-        pm.printProductReport();
+        pm.printProductReport(p1);
         p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Nice hot cup of tea!");
         p1 = pm.reviewProduct(p1, Rating.TWO_STAR, "Rather weak taste");
         p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Fine tea");
         p1 = pm.reviewProduct(p1, Rating.FOUR_STAR, "Good tea");
         p1 = pm.reviewProduct(p1, Rating.THREE_STAR, "Just add some lemon");
         p1 = pm.reviewProduct(p1, Rating.FIVE_STAR, "Perfect tea!");
-        pm.printProductReport();
+        pm.printProductReport(p1);
 //        Product p2 = pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
 //        Product p3 = pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
 //        Product p4 = pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99),Rating.TWO_STAR,LocalDate.now());
