eed05ea3a5f9ab547c71df03fa37e2f6b841732b
Omar Tapia Santacruz <omar.tsc@gmail.com>
2021-04-12 11:19:06 +0200

    use lamda expressions to test the printing of sorted products

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -18,7 +18,7 @@ package labs.pm.app;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
-import java.util.Locale;
+import java.util.Comparator;
 import labs.pm.data.Product;
 import labs.pm.data.ProductManager;
 import labs.pm.data.Rating;
@@ -38,36 +38,44 @@ public class Shop {
 
         ProductManager pm = new ProductManager("en-GB");
         pm.createProduct(101, "Tea", BigDecimal.valueOf(1.99), Rating.NOT_RATED);
-        pm.printProductReport(101);
+//        pm.printProductReport(101);
         pm.reviewProduct(101, Rating.FOUR_STAR, "Nice hot cup of tea!");
         pm.reviewProduct(101, Rating.TWO_STAR, "Rather weak taste");
         pm.reviewProduct(101, Rating.FOUR_STAR, "Fine tea");
         pm.reviewProduct(101, Rating.FOUR_STAR, "Good tea");
         pm.reviewProduct(101, Rating.THREE_STAR, "Just add some lemon");
         pm.reviewProduct(101, Rating.FIVE_STAR, "Perfect tea!");
-        pm.printProductReport(101);
+//        pm.printProductReport(101);
 
         pm.createProduct(102, "Coffee", BigDecimal.valueOf(1.99), Rating.FOUR_STAR);
         pm.reviewProduct(102, Rating.THREE_STAR, "Coffee was ok");
         pm.reviewProduct(102, Rating.ONE_STAR, "where is my milk?");
         pm.reviewProduct(102, Rating.FIVE_STAR, "Perfect with ten spoons of sugar");
-        pm.printProductReport(102);
-        
-        pm.changeLocale("de-DE");
-        
+//        pm.printProductReport(102);
+
+//        pm.changeLocale("de-DE");
+
         pm.createProduct(103, "Cake", BigDecimal.valueOf(3.99), Rating.FIVE_STAR, LocalDate.now().plusDays(2));
         pm.reviewProduct(103, Rating.FIVE_STAR, "Very nice cake");
         pm.reviewProduct(103, Rating.FOUR_STAR, "It good, but more chocolate");
         pm.reviewProduct(103, Rating.FIVE_STAR, "This cake is just perfect!");
-        pm.printProductReport(103);
-        
-        pm.changeLocale("es-MX");
-        
-        pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99),Rating.TWO_STAR,LocalDate.now());
+//        pm.printProductReport(103);
+
+//        pm.changeLocale("es-MX");
+
+        pm.createProduct(105, "Cookie", BigDecimal.valueOf(3.99), Rating.TWO_STAR, LocalDate.now());
         pm.reviewProduct(105, Rating.THREE_STAR, "just another cookie");
         pm.reviewProduct(105, Rating.FOUR_STAR, "keep it on with this cookies");
         pm.reviewProduct(105, Rating.FIVE_STAR, "best cookie ever");
-        pm.printProductReport(105);
+//        pm.printProductReport(105);
+
+        Comparator<Product> ratingSorter = (p1,p2) -> p2.getRating().ordinal() - p1.getRating().ordinal();
+        Comparator<Product> priceSorter = (p1,p2) -> p2.getPrice().compareTo(p1.getPrice());
+        pm.printProducts(ratingSorter.thenComparing(priceSorter));
+        pm.printProducts(priceSorter.thenComparing(ratingSorter).reversed());
+        
+        
+        
     }
 
 }
