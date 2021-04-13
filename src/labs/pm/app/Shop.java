37c232087e7d6b0a045bd90a7635cc4add3470fa
Omar Tapia Santacruz <omar.tsc@gmail.com>
2021-04-13 19:53:24 +0200

    test getDiscounts method

--- src/labs/pm/app/Shop.java
+++ src/labs/pm/app/Shop.java
@@ -71,6 +71,7 @@ public class Shop {
         pm.printProducts(p -> p.getPrice().floatValue() < 2, 
                 (p1,p2) -> p2.getRating().ordinal() - p1.getRating().ordinal());
 
+        pm.getDiscounts().forEach((rating, discount) -> System.out.println(rating + '\t' + discount));
 //        Comparator<Product> ratingSorter = (p1,p2) -> p2.getRating().ordinal() - p1.getRating().ordinal();
 //        Comparator<Product> priceSorter = (p1,p2) -> p2.getPrice().compareTo(p1.getPrice());
 //        pm.printProducts(ratingSorter.thenComparing(priceSorter));
