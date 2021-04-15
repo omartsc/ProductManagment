d53859b34ee43b58f9406d544e98047dc0d55eb1
Omar Tapia Santacruz <omar.tsc@gmail.com>
2021-04-15 14:47:55 +0200

    catch simultaneously numberformatexceptiojns

--- src/labs/pm/data/ProductManager.java
+++ src/labs/pm/data/ProductManager.java
@@ -144,7 +144,7 @@ public class ProductManager {
         try {
             Object[] values = reviewFormat.parse(text);
             reviewProduct(Integer.parseInt((String)values[0]), Rateable.convert(Integer.parseInt((String)values[1])), (String)values[2]);
-        } catch (ParseException ex) {
+        } catch (ParseException | NumberFormatException ex) {
             logger.log(Level.WARNING, "Error parsing review " + text, ex);
         }
     }
